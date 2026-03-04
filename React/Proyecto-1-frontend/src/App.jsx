85177e5ec6df19554fa02171365419f44ddf71e5
fabricio vargas alfaro <fabricio.12@live.com>
2026-03-04 15:25:34 -0600

    Avance 3 tienda de mascotas

--- React/Proyecto-1-frontend/src/App.jsx
+++ React/Proyecto-1-frontend/src/App.jsx
@@ -7,10 +7,15 @@ import ProductDetail from "./pages/ProductDetail";
 import { CreateP, ProductTable } from "./pages/Administration";
 import Edit from "./pages/Edit";
 import data from "./data/products.json";
+import CreateUser from "./pages/CreateUser";
+import Login from "./pages/Login";
+import Loading from "./components/Loading";
+
 function App() {
   const [page, setPage] = useState("home");
   const [productId, setProductId] = useState(null);
   const [products, setProducts] = useState(data);
+  const [loading, setLoading] = useState(false);
 
   const goTo = (newPage, id = null) => {
     setPage(newPage);
@@ -25,10 +30,13 @@ function App() {
     if (page === "detail") return <ProductDetail goTo={goTo} productId={productId} products={products} />;
     if (page === "admin") return (<><ProductTable goTo={goTo} products={products} setProducts={setProducts} /> <CreateP goTo={goTo} products={products} setProducts={setProducts} /></>);
     if (page === "edit") return <Edit goTo={goTo} productId={productId} products={products} setProducts={setProducts} />;
+    if (page === "createUser") return <CreateUser goTo={goTo} setLoading={setLoading} />;
+    if (page === "login") return <Login goTo={goTo} setLoading={setLoading} />;
   };
-
+  console.log(page);
   return (
     <>
+      {loading && <Loading />}
       <Header goTo={goTo} />
       <main>{renderPage()}</main>
       <Footer />
--- React/Proyecto-1-frontend/src/components/Header.jsx
+++ React/Proyecto-1-frontend/src/components/Header.jsx
@@ -2,22 +2,61 @@
 import "./Header.css";
 import logo from "../img/icons/lucide-PawPrint-Outlined.svg";
 function Header({ goTo }) {
-    return (
-        <header className="container">
-            <div className="logo-group">
-                <div className="logo-box">
-                    <img src={logo} alt="Paw logo" />
+    if (localStorage.getItem("role") === "admin") {
+        return (
+            <header className="container">
+                <div className="logo-group">
+                    <div className="logo-box">
+                        <img src={logo} alt="Paw logo" />
+                    </div>
+                    <h1 id="storename">PawStore</h1>
                 </div>
-                <h1 id="storename">PawStore</h1>
-            </div>
-            <nav>
-                <a className="nav-link" onClick={() => goTo("home")}>Inicio</a>
-                <a className="nav-link" onClick={() => goTo("catalog")}>Catálogo</a>
-                <a className="nav-link" onClick={() => goTo("admin")}>Administración</a>
-                <a className="nav-link" onClick={() => goTo("contact")}>Contacto</a>
-            </nav>
-        </header>
-    );
+                <nav>
+                    <a className="nav-link" onClick={() => goTo("home")}>Inicio</a>
+                    <a className="nav-link" onClick={() => goTo("catalog")}>Catálogo</a>
+                    <a className="nav-link" onClick={() => goTo("admin")}>Administración</a>
+                    <a className="nav-link" onClick={() => goTo("contact")}>Contacto</a>
+                    <a className="nav-link" onClick={() => { localStorage.clear(); goTo("home") }}>Cerrar Sesión</a>
+                </nav>
+            </header>
+        );
+    }
+    if (localStorage.getItem("role") === "user") {
+        return (
+            <header className="container">
+                <div className="logo-group">
+                    <div className="logo-box">
+                        <img src={logo} alt="Paw logo" />
+                    </div>
+                    <h1 id="storename">PawStore</h1>
+                </div>
+                <nav>
+                    <a className="nav-link" onClick={() => goTo("home")}>Inicio</a>
+                    <a className="nav-link" onClick={() => goTo("catalog")}>Catálogo</a>
+                    <a className="nav-link" onClick={() => goTo("contact")}>Contacto</a>
+                    <a className="nav-link" onClick={() => { localStorage.clear(); goTo("home") }}>Cerrar Sesión</a>
+                </nav>
+            </header>
+        );
+    }
+    else {
+        return (
+            <header className="container">
+                <div className="logo-group">
+                    <div className="logo-box">
+                        <img src={logo} alt="Paw logo" />
+                    </div>
+                    <h1 id="storename">PawStore</h1>
+                </div>
+                <nav>
+                    <a className="nav-link" onClick={() => goTo("home")}>Inicio</a>
+                    <a className="nav-link" onClick={() => goTo("catalog")}>Catálogo</a>
+                    <a className="nav-link" onClick={() => goTo("contact")}>Contacto</a>
+                    <a className="nav-link" onClick={() => goTo("login")}>Iniciar Sesión</a>
+                </nav>
+            </header>
+        );
+    }
 }
 
 export default Header;
